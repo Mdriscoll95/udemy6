7bea2631889609106e052d3e2a6ceec1e91a00ab
Michael Driscoll <michael.driscoll951@gmail.com>
2023-07-12 20:51:46 -0700

    udemy6

---
+++ udemy6/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace udemy6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //constants are immutable values which are know
+        //cannot be changed during the lifetime of the program
+
+        //constants as fields
+
+        const double PI = 3.14;
+        const int weeks = 52, months = 12;
+        //create const as string with my birthday as its value
+        const string myBirthday = "June 11 1995";
+        Console.WriteLine("my birstday is always going to be: {0}", myBirthday);
+        Console.Read();
+    }
+}
+
